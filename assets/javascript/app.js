919576781a68fe09a3df4aa636076b610b40db40
Danei Stephenson <daneistephenson@Daneis-MacBook-Air.local>
2016-11-02 22:39:07 -0400

    updated css and html skeletion

---
+++ assets/javascript/app.js
@@ -0,0 +1,48 @@
+var questions = [{
+	"question": "On What Day Did Aaron Asked Cady What The Date Of The Day Was?",
+	"option1": "November 24",
+	"option2": "January 2",
+	"option3": "October 3",
+	"option4": "April 12",
+	"answer": "3"
+
+},{
+	"question": "Complete This Quote: So you agree. You think you're really...",
+	"option1": "Pretty",
+	"option2": "Smart",
+	"option3": "Active",
+	"option4": "Cool",
+	"answer": "1"
+
+},{
+	"question": "What Was The Name of The Song The Girls Performed To During The Talent Show?",
+	"option1": "Santa Baby",
+	"option2": "Jingle Bell Rock",
+	"option3": "Grandma Got Run Over By A Raindeer",
+	"option4": "Merry Christmas",
+	"answer": "2"
+},{
+	"question": "Which Character's Father Invented Toaster Strudel ,
+	"option1": "Gretchen",
+	"option2": "Aaron",
+	"option3": "Damein",
+	"option4": "Regina",
+	"answer": "1"
+},{
+	"question": How Many Candy Cane Grams Did Glenn CoCo get?",
+	"option1": "None",
+	"option2": "2",
+	"option3": "4",
+	"option4": "5",
+	"answer": "3"
+},{
+	"question": What Did Cady Give To Regina to Help Her "lose weight"?,
+	"option1": "SlimFast Drinks",
+	"option2": "Peppermint Sticks",
+	"option3": "Perchmant Bars",
+	"option4": "Kalteen Bars",
+	"answer": "4"
+
+}
+
+]
