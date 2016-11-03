74ec2eab819468a34f120a47984e776b4d1d1fa6
Danei Stephenson <daneistephenson@Daneis-MacBook-Air.local>
2016-11-03 17:50:11 -0400

    updated answers on start over page, goes to wrong div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var questions = [{
 	"option2": "January 2",
 	"option3": "October 3",
 	"option4": "April 12",
-	"answer": "3"
+	"answer": "October 3"
 
 },{
 	"question": "Complete This Quote: So you agree. You think you're really...",
@@ -12,7 +12,7 @@ var questions = [{
 	"option2": "Smart",
 	"option3": "Active",
 	"option4": "Cool",
-	"answer": "1"
+	"answer": "Pretty"
 
 },{
 	"question": "What Was The Name of The Song The Girls Performed To During The Talent Show?",
@@ -20,35 +20,35 @@ var questions = [{
 	"option2": "Jingle Bell Rock",
 	"option3": "Grandma Got Run Over By A Raindeer",
 	"option4": "Merry Christmas",
-	"answer": "2"
+	"answer": "Jingle Bell Rock"
 },{
 	"question": "Which Character's Father Invented Toaster Strudel?",
 	"option1": "Gretchen",
 	"option2": "Aaron",
 	"option3": "Damein",
 	"option4": "Regina",
-	"answer": "1"
+	"answer": "Gretchen"
 },{
 	"question": "How Many Candy Cane Grams Did Glen CoCo get?",
 	"option1": "None",
 	"option2": "2",
 	"option3": "4",
 	"option4": "5",
-	"answer": "3"
+	"answer": "4"
 },{
 	"question": "What Did Cady Give To Regina to Help Her lose weight?",
 	"option1": "SlimFast Drinks",
 	"option2": "Peppermint Sticks",
 	"option3": "Perchmant Bars",
 	"option4": "Kalteen Bars",
-	"answer": "4"
+	"answer": "Kalteen Bars"
 
 }]
 
 var images = ["assets/images/giphy.gif", "assets/giphyPretty.gif", "assets/images/giphyJBR.gif", "assets/images/giphyTS.gif", "assets/images/giphyGC.gif", "assets/images/giphyKB.gif"];
-var rightAnswers;
-var wrongAnswers;
- var unanswered;
+var rightAnswers = 0;
+var wrongAnswers = 0;
+ var unanswered = 0;
 var count = 0;
 var timer;
 var stopwatch;
@@ -112,34 +112,49 @@ $(".gameTitle").hide();
 	$(".totalResults").show();
 
 
-
-
-
-
 }
-
-
-
-
+var index = 0;
 function loadQuestion (questionIndex) {
 
 	if (questionIndex == 6){
+		$("#rightAnswers").append(rightAnswers);
+		$("#wrongAnswers").append(wrongAnswers);
+		$("#unanswered").append(unanswered);
+		
+		
 		startover();
 
 	}
 
 	else {
+		index++
 		var q = questions[questionIndex];
 		questionEl.textContent = (questionIndex +1) + '.' +q.question;
 		opt1.textContent = q.option1;
 		opt2.textContent = q.option2;
 		opt3.textContent = q.option3;
-		opt4.textContent = q.option4;
+	    opt4.textContent = q.option4;
 	 
 	}
 	
 };
 
+$(".option").click(function(){
+	 if (this == questions[index].answer){
+	 	rightAnswers++;
+	 	console.log(rightAnswers);
+	 	loadNextQuestion();
+		t = 10;
+		$('.timer').html(t);
+	 }
+	 else {
+	 	wrongAnswers ++;
+	 	console.log(wrongAnswers);
+	 	loadNextQuestion();
+		t = 10;
+		$('.timer').html(t);
+	 }
+})
 
 $(".start").click(function(){
 	$(".gameTitle").hide();
@@ -213,7 +228,7 @@ startover();
 	loadQuestion(currentQuestion);
 
 }
-
+$("#nextButton").hide();
 
 loadQuestion(currentQuestion);
 
