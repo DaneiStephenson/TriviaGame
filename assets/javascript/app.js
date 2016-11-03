ede0cd68ee6a5d6830508b4ca06cb5c0ac54ce8f
Danei Stephenson <daneistephenson@Daneis-MacBook-Air.local>
2016-11-03 01:03:10 -0400

    have a start page, the button work, finish button print score wrong

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,27 +22,91 @@ var questions = [{
 	"option4": "Merry Christmas",
 	"answer": "2"
 },{
-	"question": "Which Character's Father Invented Toaster Strudel ,
+	"question": "Which Character's Father Invented Toaster Strudel?",
 	"option1": "Gretchen",
 	"option2": "Aaron",
 	"option3": "Damein",
 	"option4": "Regina",
 	"answer": "1"
 },{
-	"question": How Many Candy Cane Grams Did Glenn CoCo get?",
+	"question": "How Many Candy Cane Grams Did Glen CoCo get?",
 	"option1": "None",
 	"option2": "2",
 	"option3": "4",
 	"option4": "5",
 	"answer": "3"
 },{
-	"question": What Did Cady Give To Regina to Help Her "lose weight"?,
+	"question": "What Did Cady Give To Regina to Help Her lose weight?",
 	"option1": "SlimFast Drinks",
 	"option2": "Peppermint Sticks",
 	"option3": "Perchmant Bars",
 	"option4": "Kalteen Bars",
 	"answer": "4"
 
+}]
+
+var currentQuestion=0;
+var score= 0;
+var totQuestions = questions.length;
+
+var container = document.getElementById("quizContainer");
+var questionEl= document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
+
+
+
+
+function loadQuestion (questionIndex) {
+	var q = questions[questionIndex];
+	questionEl.textContent = (questionIndex +1) + '.' +q.question;
+	opt1.textContent = q.option1;
+	opt2.textContent = q.option2;
+	opt3.textContent = q.option3;
+	opt4.textContent = q.option4;
+
+};
+$("button").click(function(){
+	$(".gameTitle").hide();
+	$(".container").show();
+});
+$(".container").hide();
+
+function loadNextQuestion () {
+
+
+
+
+
+	var selectedOption = document.querySelector['input[type=radio]: checked'];
+	
+
+
+	
+	var answer = ("selectedOption").value;
+	if(questions[currentQuestion].answer == answer){
+		score +=  10;
+	}
+	("selectedOption").checked = false;
+	currentQuestion++;
+	if(currentQuestion == totQuestions - 1){
+		nextButton.textContent = "finish";
+
+	}
+	if (currentQuestion == totQuestions){
+		
+		resultCont.style.display ="";
+		resultCont.textContent= "your score" + score;
+		return;
+	}
+	loadQuestion(currentQuestion);
+
 }
 
-]
+
+loadQuestion(currentQuestion);
+
